b5206dc59a18286a79a81811b0a687c6d26c8e40
Amir Hassanzadeh <35879739+yxoos@users.noreply.github.com>
2021-06-22 11:45:58 -0400

    uploaded example gifs

--- setup.py
+++ setup.py
@@ -12,15 +12,12 @@ setup(
 	url = "",
 	author_email = "ah7557@rit.edu" ,
 	packages_dir={"":"jostar"},
-	classifiers=['Programming Language :: Python :: 3',
-        		'Programming Language :: Python :: 3.5',
-        		'Programming Language :: Python :: 3.6',
-        		'Programming Language :: Python :: 3.7',
-        		'Programming Language :: Python :: 3.8',
-        		'Programming Language :: Python :: 3 :: Only',
+	keywords={"optimization", "feature-selection","genetic algorithm", "particle swarm", "ant colony", "metaheuristics", "differential evolution", "sequential search", "multi-objective"},
+	classifiers=['Programming Language :: Python :: 3.6',
 				"License :: OSI Approved :: MIT License",
 				"Operating System :: OS Independent"],
 	install_requires=["sklearn","matplotlib","pathos","scipy","seaborn"],
-	extras_require={"dev":["pytest>=3.7",],}
+	extras_require={"dev":["pytest>=3.6",],}
 	)
 	
+
