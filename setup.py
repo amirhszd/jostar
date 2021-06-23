bc4518b096c7845acc0a1bb6a074a07c573f65e7
Amir Hassanzadeh <35879739+yxoos@users.noreply.github.com>
2021-06-23 14:46:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
 
-with open("README.md", "r", encoding="utf8") as fh:
-	long_description = fh.read()
-
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
 	name= "jostar",
 	version="0.0.1",
